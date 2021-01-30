f29c659f49810c3316df4a36087fc13c902e2ec8
Antanas Stanys <antanas@stanys.eu>
2021-01-30 10:57:13 +0200

    Added regex helper

--- app/helper.js
+++ app/helper.js
@@ -3,6 +3,9 @@ const md5 = require('md5');
 const Controller = {
     hash: password => {
         return md5(password);
+    },
+    stringRegexOverride: function (str) {
+        return str.split('(').join('\\(').split(')').join('\\)').split('+').join('\\+').split('|').join('\\|');
     }
 };
 
